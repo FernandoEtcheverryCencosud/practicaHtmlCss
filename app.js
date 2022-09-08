7a91b2bea493dabb85f3bfc99e6b96ff66780708
FernandoEtcheverry <francisco.etcheverry@cencosud>
2022-09-07 21:45:27 -0300

    Agrego todos los files del ejercicio

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const puerto = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.listen(puerto, () => {
+  console.log(`Servidor Corriendo en el puerto ${puerto}`);
+});
